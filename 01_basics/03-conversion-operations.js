8224a48469bf421f489f315d42611cfb76de9b3f
Ankita🌸 <ankietathorve@gmail.com>
2025-08-13 12:22:01 +0000

    conversion of datatypes

---
+++ 01_basics/03-conversion-operations.js
@@ -0,0 +1,33 @@
+// let score = '33abc'
+let score = null
+
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let inNumber = Number(score)
+console.log(typeof inNumber)
+
+console.log(inNumber)
+
+// '33' => 33
+// '33abc' => NaN
+// true => 1; false => 0;
+
+
+let isLoggedin = 1;
+let boolean = Boolean(isLoggedin)
+
+console.log(typeof boolean)
+console.log(boolean)
+
+// true => 1
+// false => 0
+// "" => false
+// "ankita" => true
+
+
+let someNumber = 222
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
