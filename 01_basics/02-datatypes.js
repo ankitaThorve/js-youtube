91412c4b4956409ffb47ccff3e307d07af36be9a
Ankita🌸 <ankietathorve@gmail.com>
2025-08-13 12:00:50 +0000

    datatypes

---
+++ 01_basics/02-datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3 + 3) //we are using nodejs, not browser
+
+console.log(3
+    +
+    3)  //code readability should high
+
+console.log('hello')
+
+
+let name='ankita'  //string
+let age = 18 //number
+let isLoggedIn = true  //boolean true or false
+
+// number => 2 to power 53
+// bigint
+// string => ''
+// boolean => true / false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// Objects
+
+console.log(typeof null);
+console.log(typeof undefined);
