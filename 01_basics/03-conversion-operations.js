7803ef9ea4a3f5f4d90d915ec8cc87a79b7962a4
Ankita🌸 <ankietathorve@gmail.com>
2025-08-13 15:19:38 +0000

    Datatype conversion & operations

--- 01_basics/03-conversion-operations.js
+++ 01_basics/03-conversion-operations.js
@@ -2,13 +2,13 @@
 let score = null
 
 
-console.log(typeof score)
-console.log(typeof(score))
+// console.log(typeof score)
+// console.log(typeof(score))
 
 let inNumber = Number(score)
-console.log(typeof inNumber)
+// console.log(typeof inNumber)
 
-console.log(inNumber)
+// console.log(inNumber)
 
 // '33' => 33
 // '33abc' => NaN
@@ -18,8 +18,8 @@ console.log(inNumber)
 let isLoggedin = 1;
 let boolean = Boolean(isLoggedin)
 
-console.log(typeof boolean)
-console.log(boolean)
+// console.log(typeof boolean)
+// console.log(boolean)
 
 // true => 1
 // false => 0
@@ -29,5 +29,32 @@ console.log(boolean)
 
 let someNumber = 222
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+// ************************* Operations *****************************
+let value = 3;
+let negvalue = -value;
+// console.log(negvalue);
+
+// console.log(2 + 2)
+// console.log(2 - 2)
+// console.log(2 * 2)
+// console.log(2 ** 3)
+// console.log(2 / 3)
+// console.log(2 % 3)
+
+// console.log('1' + 2)
+// console.log(1 + '2')
+// console.log('1' + 2 + 2)
+// console.log(1 + 2 + '2')
+
+// console.log(+true)
+// console.log(+"")
+
+let num1, num2, num3
+num1=num2=num3=2+2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter)
