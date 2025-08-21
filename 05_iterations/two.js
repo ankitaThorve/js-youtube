8bda366539c2bd859504f2db4e687d85ec52d861
Ankita🌸 <ankietathorve@gmail.com>
2025-08-21 18:05:35 +0000

    while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,14 @@
+let index = 0
+while (index <= 10) {
+    // console.log(`Value of index is ${index}`)
+    index = index + 2
+}
+
+let myArray = ["thor", "loki", "blackPanther", "bucky"]
+
+let arr = 0
+while (arr < myArray.length) {
+    console.log(`Value is ${myArray[arr]}`)
+    arr = arr + 1
+    // arr++
+}
