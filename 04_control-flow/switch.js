08391efa9e6beed9faeda267072312862fd15a59
Ankita🌸 <ankietathorve@gmail.com>
2025-08-21 11:04:20 +0000

    switch case statement

---
+++ 04_control-flow/switch.js
@@ -0,0 +1,36 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 8
+
+switch (month) {
+    case 1:
+        console.log('Jan')
+        break;
+    case 2:
+        console.log('Feb')
+        break;
+    case 3:
+        console.log('Mar')
+        break;
+    case 4:
+        console.log('Apr')
+        break;
+    case 5:
+        console.log('May')
+        break;
+    case 6:
+        console.log('Jun')
+        break;
+    
+
+    default:
+        console.log('default case match')
+        break;
+}
