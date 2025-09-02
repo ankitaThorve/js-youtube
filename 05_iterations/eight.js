69a387bd73a2c37d47557868b17a3143bae33e6c
Ankita🌸 <ankietathorve@gmail.com>
2025-09-02 12:57:33 +0000

    array.reduce()

---
+++ 05_iterations/eight.js
@@ -0,0 +1,28 @@
+const myNum = [1,2,3]
+
+// const myTotal = myNum.reduce(function (acc, currval){
+//     console.log(`acc: ${acc} and currval: ${currval}`)
+//     return acc + currval
+// }, 0)
+
+const myTotal = myNum.reduce((acc, curr)=>(acc + curr),0)
+// console.log(myTotal)
+
+const shoppingCart = [
+    {
+        itemName: 'js course',
+        price: 2999,
+    },
+    {
+        itemName: 'py course',
+        price: 999,
+    },
+    {
+        itemName: 'dsa course',
+        price: 10999,
+    }
+]
+
+const priceToPay = shoppingCart.reduce((acc, item)=>acc + item.price,0)
+
+console.log(priceToPay)
